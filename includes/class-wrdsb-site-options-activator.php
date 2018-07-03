f770297d80345d8258824f3a7fc2b63d79c8b946
James Schumann <jschumann@gmail.com>
2018-07-03 09:36:51 -0400

    generate wordpress plugin boilerplate

---
+++ includes/class-wrdsb-site-options-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/wrdsb
+ * @since      1.0.0
+ *
+ * @package    Wrdsb_Site_Options
+ * @subpackage Wrdsb_Site_Options/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wrdsb_Site_Options
+ * @subpackage Wrdsb_Site_Options/includes
+ * @author     WRDSB <website@wrdsb.ca>
+ */
+class Wrdsb_Site_Options_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wrdsb-site-options-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/wrdsb
+ * @since      1.0.0
+ *
+ * @package    Wrdsb_Site_Options
+ * @subpackage Wrdsb_Site_Options/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wrdsb_Site_Options
+ * @subpackage Wrdsb_Site_Options/includes
+ * @author     WRDSB <website@wrdsb.ca>
+ */
+class Wrdsb_Site_Options_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
